6be2b72dee1f3eb2543d75c97478e7b090227f94
Janhavi Bhangale <bhangale@uark.edu>
2026-03-02 10:49:49 -0600

    Hello world

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+# prompt the user to input their name
 name = input ("enter your name: ")
 
-print('hello world')
+# say 'hello world'
 print ('hello',name)
+
+# print greeting with the user's name
+print ('hello', name)
 
 
